8966baf4d1600308aa8cde2d4b25ce7c14b24fe1
Sagar Rajput <sagar.rajput9753@gmail.com>
2023-02-28 20:58:06 +0530

    Update 1572. Matrix Diagonal Sum.java

--- 1572. Matrix Diagonal Sum.java
+++ 1572. Matrix Diagonal Sum.java
@@ -1,5 +1,5 @@
-/*-----------------------------------------------Question------------------------------------
-************> 2d Array (Easy) <************
+/*************> 2d Array (Easy) <************
+-----------------------------------------------Question------------------------------------
 
 Given a square matrix mat, return the sum of the matrix diagonals.
 Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
