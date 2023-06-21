2ce1fb6fc19fd67c4d3bab46589bc0403af51674
Sagar Rajput <sagar.rajput9753@gmail.com>
2023-06-21 08:07:08 +0530

    Create 2540. Minimum Common Value.java

---
+++ 2540. Minimum Common Value.java
@@ -0,0 +1,23 @@
+------------------------------------------------- Array (Easy) ----------------------------------------------
+  
+Ques Link - https://leetcode.com/problems/minimum-common-value/description/
+
+------------------------------------------------ Solution --------------------------------------------------
+  
+class Solution {
+  public int getCommon(int[] nums1, int[] nums2) {
+    int i = 0;
+    int j = 0; 
+
+    while (i < nums1.length && j < nums2.length) {
+      if (nums1[i] == nums2[j])
+        return nums1[i];
+      if (nums1[i] < nums2[j])
+        i++;
+      else
+        j++;
+    }
+
+    return -1;
+  }
+}
