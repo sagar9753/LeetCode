6d04a1870d486d481336ba4faa99e9143faca8ed
Sagar Rajput <sagar.rajput9753@gmail.com>
2023-07-06 18:11:12 +0530

    Create 2011. Final Value of Variable After Performing Operations.java

---
+++ 2011. Final Value of Variable After Performing Operations.java
@@ -0,0 +1,18 @@
+Ques link - https://leetcode.com/problems/final-value-of-variable-after-performing-operations/description/
+
+-------------------------------------------------------- Solution --------------------------------------------
+
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int sum = 0;
+
+        for(int i = 0 ; i < operations.length ; i++){
+            if(operations[i].charAt(1) == '+')
+                sum += 1;
+            else 
+                sum += -1;
+        }
+        return sum;
+
+    }
+}
