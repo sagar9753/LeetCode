91e0a43ab4f23f277decc5db45303a15e06e1a77
Sagar Rajput <sagar.rajput9753@gmail.com>
2023-08-03 22:03:07 +0530

    Create 1816. Truncate Sentence.java

---
+++ 1816. Truncate Sentence.java
@@ -0,0 +1,15 @@
+Ques Link - https://leetcode.com/problems/truncate-sentence/description/
+
+------------------------------------------- Solution ------------------------------------------------
+
+class Solution {
+    public String truncateSentence(String s, int k) {
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i) == ' ')
+                k--;
+            if(k == 0)
+                return s.substring(0,i);
+        }
+        return s;
+    }
+}
