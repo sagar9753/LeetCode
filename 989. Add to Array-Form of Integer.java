8f782501d8ca614a4c84de09dd3b9942ef20b861
Sagar Rajput <sagar.rajput9753@gmail.com>
2023-10-10 20:06:54 +0530

    Create 989. Add to Array-Form of Integer.java

---
+++ 989. Add to Array-Form of Integer.java
@@ -0,0 +1,21 @@
+Ques Link - https://leetcode.com/problems/add-to-array-form-of-integer/
+
+------------------------------------------- Solution -------------------------------------------------------
+
+class Solution {
+  public List<Integer> addToArrayForm(int[] num, int k) {
+    List<Integer> ans = new LinkedList<>();
+
+    for (int i = num.length - 1; i >= 0; --i) {
+      ans.add(0, (num[i] + k) % 10);
+      k = (num[i] + k) / 10;
+    }
+
+    while (k > 0) {
+      ans.add(0, k % 10);
+      k /= 10;
+    }
+
+    return ans;
+  }
+}
