d082feaefcf1bbd4c0fef868eb5319d2ae410279
Sagar Rajput <sagar.rajput9753@gmail.com>
2023-11-09 17:11:47 +0530

    Create 334. Increasing Triplet Subsequence.java

---
+++ 334. Increasing Triplet Subsequence.java
@@ -0,0 +1,20 @@
+Ques Link - https://leetcode.com/problems/increasing-triplet-subsequence
+
+-------------------------------------- Solution -------------------------------------------
+
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int num1 = Integer.MAX_VALUE;
+        int num2 = Integer.MAX_VALUE;
+
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] <= num1)
+                num1 = nums[i];
+            else if(nums[i] <= num2)
+                num2 = nums[i];
+            else
+                return true;
+        }
+        return false;
+    }
+}
