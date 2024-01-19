bd4f8658d26956ac4ce4753817db2c43c8914df6
Sagar Rajput <sagar.rajput9753@gmail.com>
2024-01-19 11:16:09 +0530

    Update 931. Minimum Falling Path Sum.java

--- 931. Minimum Falling Path Sum.java
+++ 931. Minimum Falling Path Sum.java
@@ -1,6 +1,6 @@
 Ques Link - https://leetcode.com/problems/minimum-falling-path-sum
 
----------------------------------------------------- Solution ------------------------------------------------------------
+----------------------------------------- Solution 1(DP Tabulation) ------------------------------------------------------------
 
 class Solution {
     public int minFallingPathSum(int[][] mat) {
@@ -31,6 +31,29 @@ class Solution {
 
         for(int i = 0 ; i < mat.length ; i++){
             ans = Math.min(ans,dp[mat.length-1][i]);
+        }
+        return ans;
+    }
+    
+}
+
+--------------------------------------------------- Solution (DP Space Optimization) ----------------------------------------------------
+
+class Solution {
+    public int minFallingPathSum(int[][] mat) {
+        int ans = Integer.MAX_VALUE;
+        
+        for(int i = 1 ; i < mat.length ; i++){
+            for(int j = 0 ; j < mat.length ; j++){
+                int min = (int) Math.pow(10, 9);
+                for(int k = Math.max(0,j-1) ; k < Math.min(mat.length,j+2) ; k++)
+                    min = Math.min(min,mat[i-1][k]);
+                mat[i][j] += min;
+            }
+        }
+
+        for(int i = 0 ; i < mat.length ; i++){
+            ans = Math.min(ans,mat[mat.length-1][i]);
         }
         return ans;
     }
