3742cdc460dd5146f710185ba4bce9c22429d3ce
Sagar Rajput <sagar.rajput9753@gmail.com>
2024-02-02 19:48:02 +0530

    Create 290. Word Pattern.java

---
+++ 290. Word Pattern.java
@@ -0,0 +1,27 @@
+Ques Link - https://leetcode.com/problems/word-pattern
+
+------------------------------------------ Solution ----------------------------------------------
+
+class Solution {
+    public boolean wordPattern(String pat, String s) {
+        String[] str = s.split(" ");
+        if(pat.length() != str.length) return false;
+
+        HashMap<Character, String> map = new HashMap();
+
+        for(int i = 0 ; i < pat.length(); i++){
+            char c = pat.charAt(i);
+            if(map.containsKey(c)){
+                if(!map.get(c).equals(str[i]))
+                    return false;
+            }
+            else{
+                if(map.containsValue(str[i]))
+                    return false;
+                map.put(c,str[i]);
+            }
+        }
+        return true;
+
+    }
+}
