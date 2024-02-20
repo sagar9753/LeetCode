ff3a3ce4acd5f6dcbc9a191f0771cd10eec77416
Sagar Rajput <sagar.rajput9753@gmail.com>
2024-02-20 10:31:30 +0530

    Update 62. Unique Paths.java

--- 62. Unique Paths.java
+++ 62. Unique Paths.java
@@ -1,6 +1,6 @@
 Ques Link - https://leetcode.com/problems/unique-paths
 
------------------------------------------- Solution -----------------------------------------------
+------------------------------------------ Solution (Memoization) -----------------------------------------------
 
 class Solution {
     public int uniquePaths(int m, int n) {
@@ -21,5 +21,29 @@ class Solution {
         int left = solve(m,n-1,dp);
 
         return dp[m][n] = up + left;
+    }
+}
+
+------------------------------------------ Solution (Tabulation) -----------------------------------------------
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] prev = new int[n];
+        Arrays.fill(prev,1);
+        
+        for(int i = 1 ; i < m ; i++){
+            int cur[] = new int[n];
+            for(int j = 0 ; j < n ; j++){
+                int up = prev[j];
+
+                int left = 0;
+                if(j > 0)
+                    left = cur[j-1];
+
+                cur[j] = up + left;
+            }
+            prev = cur;
+        }
+        return prev[n-1];
     }
 }
